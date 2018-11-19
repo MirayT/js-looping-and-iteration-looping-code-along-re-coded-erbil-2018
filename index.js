8776df319f78a648aaa39e464858e750540bb85c
Miray Talal Bko <meeray.k93@gmail.com>
2018-11-19 20:03:30 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+let names= ['Ada', 'Brendan', 'Ali']
+function printBadges(names){ for (let i=0; i<names.length; i++) {console.log(`Employee ${names[i]} Badges`)}
+                           return names;}
+// tailsNeverFails
+ tail_counter=0
+function givemerandom() {
+  return Math.random();
+}
+function tailsNeverFails(){
+while(givemerandom>=0.5){
+tail_counter++}
+console.log(`You got ${tail_counter}tails in a raw`)
+}
